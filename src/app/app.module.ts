96cbf19fef1e470d853eb5000c75898dd576f144
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-11-27 18:04:13 +0300

    add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -8,30 +7,35 @@ import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatMenuModule } from '@angular/material/menu';
 
-  @NgModule({
-    declarations: [AppComponent],
-    imports: [
-      BrowserModule,
-      BrowserAnimationsModule,
-      MatSidenavModule,
-      MatGridListModule,
-      MatButtonModule,
-      MatCardModule,
-      MatIconModule,
-      MatExpansionModule,
-      MatListModule,
-      MatToolbarModule,
-      MatTableModule,
-      MatBadgeModule,
-      MatSnackbarModule,
-    ],
-    providers: [],
-    bootstrap: [AppComponent],
-  })
-  export (class AppModule {});
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatExpansionModule,
+    MatListModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatBadgeModule,
+    MatSnackBarModule,
+    MatMenuModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
