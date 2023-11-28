d89dc5fe7223a3a2a2cb1f3e849cc1288167d86a
arianoorqorbani <aria.noorghorbani@gmail.com>
2023-11-28 16:23:46 +0300

    add functionality to header product

--- src/app/home/products-header/products-header.component.ts
+++ src/app/home/products-header/products-header.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-products-header',
   templateUrl: './products-header.component.html',
-  styleUrls: ['./products-header.component.scss']
+  styleUrls: ['./products-header.component.scss'],
 })
 export class ProductsHeaderComponent {
+  @Output() columnCountChanged = new EventEmitter<number>();
 
+  sort = 'desc';
+  count = 12;
+
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort;
+  }
+
+  onCountMenu(newCount: number): void {
+    this.count = newCount;
+  }
+
+  onChangeColumn(colsNum: number): void {
+    this.columnCountChanged.emit(colsNum);
+  }
 }
